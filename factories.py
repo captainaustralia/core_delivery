d7445c16bfdb0b09be01851f97afe2bc3abe054e
Sandro <physrow@gmail.com>
2023-02-27 21:40:13 +0400

    core-delivery-2: add DefaultUserFactory

---
+++ factories.py
@@ -0,0 +1,13 @@
+import factory
+
+from core_delivery.users.models import DefaultUser
+
+
+class DefaultUserFactory(factory.django.DjangoModelFactory):
+    first_name = factory.Sequence(lambda n: 'FirstName%d' % n)
+    last_name = factory.Sequence(lambda n: 'LastName%d' % n)
+    phone = factory.Sequence(lambda n: '+7909457%03d' % n)
+    password = "strongpassword"
+
+    class Meta:
+        model = DefaultUser
