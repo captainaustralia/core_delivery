285a5504d3ba0bdf0cdb13613fe823ef7b9eccfb
Sandro <physrow@gmail.com>
2023-02-28 21:15:25 +0400

    core-delivery-3: change import

--- orders/rest/urls.py
+++ orders/rest/urls.py
@@ -1,9 +1,8 @@
 from rest_framework.routers import SimpleRouter
 
-from core_delivery.base.rest.views import OrderViewSet
+from core_delivery.orders.rest.views import OrderViewSet
 
 router = SimpleRouter()
 router.register(basename="order", viewset=OrderViewSet, prefix="")
-
 
 urlpatterns = [] + router.urls
