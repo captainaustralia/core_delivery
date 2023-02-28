a0947933ae7b7fdc40cc12a3ec11ce464c42e6dc
Sandro <physrow@gmail.com>
2023-02-28 21:14:16 +0400

    core-delivery-3: remove OrderSerializer, create PassportDataSerializer

--- base/rest/serializers.py
+++ base/rest/serializers.py
@@ -1,17 +1,26 @@
-from rest_framework import serializers
+import datetime
 
-from core_delivery.orders.models import Order
+from rest_framework.serializers import ModelSerializer
+from rest_framework.exceptions import ValidationError
+from core_delivery.base.models import PassportData
 
 
-class OrderSerializer(serializers.ModelSerializer):
+class PassportDataSerializer(ModelSerializer):
     class Meta:
-        model = Order
-        fields = (
-            "content",
-            "description",
-            "dispatch_point",
-            "delivery_point",
-            "fragile",
-            "weight",
-            "dimension",
-        )
+        model = PassportData
+        exclude = ('date_modified', 'date_created', 'deleted')
+        read_only_fields = ('verify',)
+
+    @staticmethod
+    def validate_expired_date(date):
+        print(date, datetime.datetime.now().date())
+        if date + datetime.timedelta(days=30) >= datetime.datetime.now().date():
+            raise ValidationError("Document was expired, or expire within 30 days!")
+        return date
+
+    @staticmethod
+    def validate_date_of_birth(date: datetime.datetime):
+        print(date.year, datetime.datetime.now().year)
+        if datetime.datetime.now().year - date.year < 0:
+            raise ValidationError("You must be of legal age")
+        return date
