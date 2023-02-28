5fd76034b4f0bb74c8415934d5a9251362887ee2
Sandro <physrow@gmail.com>
2023-02-28 21:16:26 +0400

    core-delivery-3: add endpoint for add pass data

--- users/rest/views.py
+++ users/rest/views.py
@@ -1,9 +1,11 @@
+from django.db import transaction
 from rest_framework import permissions
 from rest_framework.decorators import action
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
+from core_delivery.base.rest.serializers import PassportDataSerializer
 from core_delivery.users.exceptions import AlreadyDeliveryException
 from core_delivery.users.models import DefaultUser, DeliveryMan
 from core_delivery.users.permissions import (OnlyAdminPermission,
@@ -55,20 +57,29 @@ class DeliveryManViewSet(ModelViewSet):
 
     queryset = DeliveryMan.objects.select_related("user", "passport", "transport").all()
 
-    def perform_create(self, serializer):
+    def perform_create(self, serializer) -> None:
         user = self.request.user
         if self._is_user_already_delivery_man(user):
             raise AlreadyDeliveryException
         serializer.save(user=user)
 
-    @staticmethod
-    def _is_user_already_delivery_man(user):
-        return hasattr(user, "deliveryman")
+    @transaction.atomic()
+    @action(methods=["post"],
+            url_path="add_pass_data",
+            detail=True,
+            permission_classes=[permissions.IsAuthenticated],
+            serializer_class=PassportDataSerializer)
+    def add_passport_data(self, request, **kwargs) -> Response:
+        deliveryman = self.get_object()
 
-    # @action(methods=["post"])
-    # def add_transport(self):
-    #     ...
-    #
-    # @action(methods=["post"])
-    # def add_passport(self):
-    #     ...
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        passport_obj = serializer.save()
+
+        deliveryman.passport = passport_obj
+        deliveryman.save()
+        return Response(data=serializer.data, status=201)
+
+    @staticmethod
+    def _is_user_already_delivery_man(user) -> bool:
+        return hasattr(user, "deliveryman")
